f424db90790351f1116aa50fd2b59bf2cc2c3afd
Amro <2592997@students.wits.ac.za>
2024-05-28 13:26:25 +0200

    NavBar Done

--- Java/navigationBar.js
+++ Java/navigationBar.js
@@ -1,20 +1,23 @@
 const navPages = [
-    {name:"Home" , href: '/SampleWebsite/index.html'}
-] 
-
+    {name: "Home" , href: '/SampleWebsite/index.html'},
+    {name: "About Me", href: '/SampleWebsite/AboutMeFolder/aboutMe.html'}
+];
 
 export function loadNavbar(CurrentPageName){
-    const nav = document.querySelector(header > nav);
-    const ul = document.createAttribute("ul");
-    for(let navPages of navPages){
-        const li = document.createAttribute("li");
-        if (CurrentPageName != navPages.name){
-            const a = document.createAttribute("a");
-            a.innerText = navPages.name;
-            a.SetAttribute("href", navPages.href);
-            li.appendChild(a);
-            ul.appendChild(li);
+    const nav = document.querySelector("header > nav");
+    const ul = document.createElement("ul");
+    for(let page of navPages){
+        const li = document.createElement("li");
+        if (CurrentPageName != page.name){
+            const a = document.createElement("a");
+            a.innerText = page.name;
+            a.setAttribute("href", page.href);
+            li.appendChild(a);            
+        } 
+        else{
+            li.innerText = page.name;
         }
-        nav.appendChild(ul);
+        ul.appendChild(li);        
     }
+    nav.appendChild(ul);
 }
